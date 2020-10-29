4ccb7ecf03084db9c78957b41b342fafc02b4c2f
Shani Black <shaniblack2004@gmail.com>
2020-10-29 15:09:43 -0500

    had error after adding additional characters in last push, so deleted them

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-let specialChar = "!@#$%^&*()'-,./?=+:;<>[]{}~`|\".split("");
+let specialChar = "!@#$%^&*()".split("");
 let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let pwdArray = []
